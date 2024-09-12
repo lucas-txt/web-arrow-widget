294aa5561b7ffda9a303a1f95d8afd53a6d5aab3
Lucas PS <lucaspsanhudo@gmail.com>
2024-09-12 18:42:31 -0300

    Adição de um README.md e refatoração no javascript

---
+++ README.md
@@ -0,0 +1,5 @@
+# Widget para esconder frases
+
+Projeto feito em HTML&CSS e JavaScript para exercitar conhecimentos
+
+https://web-arrow-widget.vercel.app/
--- script.js
+++ script.js
@@ -1,54 +1,24 @@
-var perguntas = document.getElementsByClassName('triangulo')
-
-
-
-perguntas.forEach(element => {
-   element.addEventListener('mousemove', () => {mexeu(element)})
-   element.addEventListener('mouseout', () => {saiu(element)})
-});
-
-function mexeu(element) {
-    element.style.width = '70%'
-    element.style.clipPath = 'polygon(0% 0%, 90% 0, 100% 50%, 90% 100%, 0% 100%)' 
+var perguntasSeta = document.getElementsByClassName('triangulo')
+function mexeuSetas(event) {
+    event.currentTarget.style.width = '90%'
+    event.currentTarget.style.clipPath = 'polygon(0% 0%, 90% 0, 100% 50%, 90% 100%, 0% 100%)'
+}
+function saiuSetas(event) {
+    event.currentTarget.style.width = '20%'
+    event.currentTarget.style.clipPath = 'polygon(0% 0%, 75% 0%, 100% 50%, 75% 100%, 0% 100%)'
 }
 
-function saiu(element) {
-    element.style.width = '20%'
-    element.style.clipPath = 'polygon(0% 0%, 75% 0%, 100% 50%, 75% 100%, 0% 100%)'
+
+
+var perguntasRetangulo = document.getElementsByClassName("retangulo")
+function mexeuRetangulo(event) {
+    event.currentTarget.style.width = '70%'
 }
-        // perguntas[0].addEventListener('mousemove', mexeu_0)
-        // perguntas[0].addEventListener('mouseout', saiu_0)
-        // perguntas[1].addEventListener('mousemove', mexeu_1)
-        // perguntas[1].addEventListener('mouseout', saiu_1)
-        // perguntas[2].addEventListener('mousemove', mexeu_2)
-        // perguntas[2].addEventListener('mouseout', saiu_2)
+function saiuRetangulo(event) {
+    event.currentTarget.style.width = '20%'
+}
 
-        // function mexeu_0() {
-        //     perguntas[0].style.width = '70%'
-        //     perguntas[0].style.clipPath = 'polygon(0% 0%, 90% 0, 100% 50%, 90% 100%, 0% 100%)' 
-        // }
-
-        // function saiu_0() {
-        //     perguntas[0].style.width = '20%'
-        //     perguntas[0].style.clipPath = 'polygon(0% 0%, 75% 0%, 100% 50%, 75% 100%, 0% 100%)'
-        // }
-
-        // function mexeu_1() {
-        //     perguntas[1].style.width = '70%'
-        //     perguntas[1].style.clipPath = 'polygon(0% 0%, 90% 0, 100% 50%, 90% 100%, 0% 100%)'
-        // }
-
-        // function saiu_1() {
-        //     perguntas[1].style.width = '20%'
-        //     perguntas[1].style.clipPath = 'polygon(0% 0%, 75% 0%, 100% 50%, 75% 100%, 0% 100%)'
-        // }
-        
-        // function mexeu_2() {
-        //     perguntas[2].style.width = '70%'
-        //     perguntas[2].style.clipPath = 'polygon(0% 0%, 90% 0, 100% 50%, 90% 100%, 0% 100%)'
-        // }
-
-        // function saiu_2() {
-        //     perguntas[2].style.width = '20%'
-        //     perguntas[2].style.clipPath = 'polygon(0% 0%, 75% 0%, 100% 50%, 75% 100%, 0% 100%)'
-        // }
+for (var i = 0; i < perguntasSeta.length; i++) {
+    perguntasSeta[i].addEventListener("mousemove", mexeuSetas)
+    perguntasSeta[i].addEventListener("mouseout", saiuSetas)
+}
